162052040494a8c8c746642d7a59d27e6d5a655a
Igor Mucsicska <mucsi_96@yahoo.com>
2020-07-29 21:54:00 +0200

    Add type safety for translation ids

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -2,7 +2,9 @@ import en from "./messages_en.json";
 import fr from "./messages_fr.json";
 import { translate } from "./utils";
 
+type ids = keyof typeof en;
+
 const bundles = { en, fr };
 export const languages = Object.keys(bundles);
-export const t = (id: string, params: Record<string, string> = {}) =>
+export const t = (id: ids, params: Record<string, string> = {}) =>
   translate(bundles, id, params);
