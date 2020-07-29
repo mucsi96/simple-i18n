190acebcb7f0772edd1e7264366e7dd9f976d673
Igor Mucsicska <mucsi_96@yahoo.com>
2020-07-29 22:03:10 +0200

    Improve naming

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -2,9 +2,9 @@ import en from "./messages_en.json";
 import fr from "./messages_fr.json";
 import { translate } from "./utils";
 
-type ids = keyof typeof en;
+type Id = keyof typeof en;
 
 const bundles = { en, fr };
 export const languages = Object.keys(bundles);
-export const t = (id: ids, params: Record<string, string> = {}) =>
+export const t = (id: Id, params: Record<string, string> = {}) =>
   translate(bundles, id, params);
